f1588c25422d05969e00bddd7315b3e2582f6693
KOMAX233 <fajasmine@outlook.com>
2025-07-31 00:04:00 -0400

    add react fetch test and fix cors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pong, setPong] = useState<Boolean | null>(null)
 
+  useEffect(() => {
+    fetch('http://localhost:8000/ping')
+    .then(async res => {
+      if (!res.ok) throw new Error(`HTTP ${res.status}`);
+      return res.json()
+    })
+    .then(data => setPong(data.pong))
+    .catch(err => {
+      console.error('Error fetch /ping: ', err)
+      setPong(false)
+    }
+    )
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>Blurry Chat Search</h1>
+      <p>
+        Ping response: {' '}
+        { pong === null? "Loading": pong? "PONG!": "NO PONG" }
       </p>
     </>
   )
